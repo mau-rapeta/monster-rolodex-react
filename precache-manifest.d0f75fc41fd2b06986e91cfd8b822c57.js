5b999883ca5d9b33e13c0dc3c7d71e4f8486d431
Mau Rapeta <mauriciorapeta147@gmail.com>
2020-02-25 17:42:29 -0600

    Updates

---
+++ precache-manifest.d0f75fc41fd2b06986e91cfd8b822c57.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "f4b23fe185e9f23691dbdfe564b41157",
+    "url": "/monsters-rolodex-react/index.html"
+  },
+  {
+    "revision": "cccc73c7fa28eff60bf5",
+    "url": "/monsters-rolodex-react/static/css/main.a41e0b53.chunk.css"
+  },
+  {
+    "revision": "c68e14fdc7e1ba4a5621",
+    "url": "/monsters-rolodex-react/static/js/2.9e7d0785.chunk.js"
+  },
+  {
+    "revision": "d705cb622423d72c5defbf368ca70dcc",
+    "url": "/monsters-rolodex-react/static/js/2.9e7d0785.chunk.js.LICENSE"
+  },
+  {
+    "revision": "cccc73c7fa28eff60bf5",
+    "url": "/monsters-rolodex-react/static/js/main.ac0f9328.chunk.js"
+  },
+  {
+    "revision": "4541c485d6a24b9691d3",
+    "url": "/monsters-rolodex-react/static/js/runtime-main.35ce1354.js"
+  }
+]);
